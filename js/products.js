61973376253b0c9a7cd37ec7047cddac49342789
Hernan Bonet <bonet.hernan@gmail.com>
2021-08-22 15:59:15 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,6 +1,44 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function () {
+    getProducts(PRODUCTS_URL)
 });
+
+async function getProducts(url) {
+    let resultObj = await getJSONData(url);
+    if (resultObj.status === 'ok') {
+        showProductList(resultObj.data);
+    } else {
+        console.log(resultObj.status);
+    }
+}
+
+function showProductList(productList) {
+    let htmlContentToAppend = "";
+    productList.forEach(({
+        cost,
+        currency,
+        description,
+        imgSrc,
+        name,
+        soldCount
+    }) => {
+        htmlContentToAppend += `
+        <a href="#" class="col-sm-12 col-md-5 col-lg-3 col-xl-2 product-card">
+					<img src="${imgSrc}" class="product-img">
+					<div class="product-card-body">
+						<h4>${name}</h4>
+						<span>${currency} ${cost}</span>
+						<hr>
+						<p>${description}</p>
+						<div>
+							<hr>
+							<small>${soldCount} vendidos</small>
+						</div>
+					</div>
+				</a>
+        `;
+    });
+    document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
+}
