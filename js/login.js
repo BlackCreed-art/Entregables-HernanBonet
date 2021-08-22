8c4b40fbb89978d2f6b70b667022e2654be39061
Hernan Bonet <bonet.hernan@gmail.com>
2021-08-22 15:17:42 -0300

    sas

--- js/login.js
+++ js/login.js
@@ -3,4 +3,25 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+}); 
+
+const userName = document.getElementById('user');
+const userPassword = document.getElementById('pwd');
+const submitButton = document.getElementById('submit');
+
+submitButton.addEventListener('click', evento =>{
+    evento.preventDefault();
+
+    let user = userName.value;
+    let password = userPassword.value;
+    
+    if(user.length >= 6 && user.length <= 8 && password.length >= 6 && password.length <= 8){
+        alert('Acceso correcto!');
+
+        location.href = './productos.html';
+    }
+    else{
+        alert('Usuario y contraseña deben estar entre 6 y 8 caracteres de largo');
+        document.getElementById('login').reset();
+    }
 });
